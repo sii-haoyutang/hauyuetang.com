57af917ea537c2ffb8165e0df19f5b14ce7e36ea
sii-haoyutang <253107030016@sii.edu.cn>
2026-05-06 17:23:50 +0800

    Add markdown math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://hauyuetang.com",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
